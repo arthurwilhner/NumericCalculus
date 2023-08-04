a9e771f465833b048e77a8cc86e9656e39f10a9e
Guilherme Arthur Wilhner <guili_arthur@hotmail.com>
2023-08-03 21:44:12 -0300

    Exercício 2.9

--- Factorial.c
+++ Factorial.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define e 2.7182818284590452
+double res;
+
+double fatorial (int n){
+    double fat;
+    if(n == 0) fat++;
+    for(fat=1;n>1;n--) {
+         fat = n * fat;
+     }
+    return fat;
+};
+
+double eulere (double n){
+    for(int num=0;num<n;num++){
+        res = res + 1/ fatorial(num);
+        printf("%.20f\n", e-(res+1));
+    }
+};
 
 int main() {
-    int fat, n=3;
-    for(fat=1;n>1;n--){
-        fat = fat*n;
-    }
-    printf("%d\n", fat);
+    eulere(20);
 }
