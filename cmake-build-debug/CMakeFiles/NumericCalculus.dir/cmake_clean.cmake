0de78bfaace52d374f545ea6be2a0376a3631de8
Guilherme Arthur Wilhner <guili_arthur@hotmail.com>
2023-08-10 21:19:06 -0300

    Cap 3.8

---
+++ cmake-build-debug/CMakeFiles/NumericCalculus.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumericCalculus.dir/Cap3.8.c.obj"
+  "CMakeFiles/NumericCalculus.dir/Cap3.8.c.obj.d"
+  "NumericCalculus.exe"
+  "NumericCalculus.exe.manifest"
+  "NumericCalculus.pdb"
+  "libNumericCalculus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NumericCalculus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
