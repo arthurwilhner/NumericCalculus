a56895f93ca3df9c74587e81d1b7f1a3c367acc1
Guilherme Arthur Wilhner <guili_arthur@hotmail.com>
2023-08-17 19:02:40 -0300

    Capítulo 3.8

--- Cap3.8.c
+++ Cap3.8.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <locale.h>
-double res, rel;
+#define max_i 50
+#define max_j 50
+#define max_v 50
+
+void imprime_Matriz(){
+
+}
+
+void recebe_Matriz(){
+
+}
+
+void imprime_Vet(){
+
+}
+
+void recebe_Vet(){
+
+}
+
+void subst_retroativa(){
+
+}
 
 int main() {
+    double M[max_i][max_j], b[max_i], a[max_j];
+    int order;
 
+    printf("Digite a ordem do sistema:\n");
+    scanf("%d\n", &order);
+    printf("Digite os elementos da Matriz: \n");
+    // Função pega matriz
+    printf("Digite o vetor B:\n");
+    // Função imprime vetor
+    printf("\nVetor X:\n");
+    // Função imprime vetor
+    printf("\n\n");
+    return 0;
 }
